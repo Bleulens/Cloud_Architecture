7038f3435ae91a3cce1321b5ee505de9a7bda754
Bleulens <popman90@gmail.com>
2025-04-19 23:46:08 -0400

    refactor: Moved region variable from module to global.

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -1,5 +1,8 @@
+# AWS region for deploying cloud resources
+# Determines the region where all infrastructure components will be provisioned.
+# Default is set to "us-east-1," but can be overridden per deployment.
 variable "aws_region" {
-  description = "AWS region where resources are deployed"
+  description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-1" # Change based on preference
 }
