7fb341c3adf5502b3b7bf899e1454bf14896d048
Bleulens <popman90@gmail.com>
2025-05-09 19:20:01 -0400

    refact: change outputs to reflect changes in new file structure

---
+++ Modules/network/outputs.tf
@@ -0,0 +1,29 @@
+# Outputs the VPC ID.
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+# Outputs IDs for public subnets.
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = [for subnet in aws_subnet.dynamic_subnets : subnet.id if subnet.tags["Type"] == "Public"]
+}
+
+# Outputs IDs for private subnets.
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = [for subnet in aws_subnet.dynamic_subnets : subnet.id if subnet.tags["Type"] == "Private"]
+}
+
+# Outputs the Internet Gateway ID (if created).
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = length(aws_internet_gateway.igw) > 0 ? aws_internet_gateway.igw[0].id : null
+}
+
+# Outputs the NAT Gateway ID (if created).
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].id : null
+}
