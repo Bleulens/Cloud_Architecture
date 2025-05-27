63a454593d40dd1037555971849042ea099a0394
Bleulens <popman90@gmail.com>
2025-05-27 14:36:06 -0400

    refact: Change main to providers, add terraform version requirements.

---
+++ Infrastructure/providers.tf
@@ -0,0 +1,21 @@
+# Configure the AWS provider
+provider "aws" {
+  region = var.aws_region # Dynamically sets AWS region from `variables.tf`
+}
+
+# Specify Terraform provider requirements
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.98.0" # Ensures compatibility with AWS provider version 5.x
+    }
+  }
+}
+
+# Define required Terraform version
+terraform {
+  required_version = "~> 1.3.7" # Ensures Terraform version 1.3.x
+}
+
+
